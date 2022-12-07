29ec6bf674834dbd0e3b80eb7e439e541a274ec6
driuft <air.socarras@gmail.com>
2022-12-06 23:16:22 -0500

    [day01] Completed solution for both parts

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,43 @@
+import kotlin.math.max
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val inputList = input.map { it.toIntOrNull() }
+
+        var currMax = 0
+        var result = 0
+        for (calories in inputList) {
+            if (calories == null) {
+                currMax = 0
+            } else {
+                currMax += calories
+                result = max(currMax, result)
+            }
+        }
+
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val inputList = input.map { it.toIntOrNull() }
+        val calorieList = mutableMapOf<Int, Int>()
+
+        var currGroup = 1
+        var currMax = 0
+        for (idx in inputList.indices) {
+            if (inputList[idx] == null) {
+                currGroup += 1
+                currMax = 0
+            } else {
+                currMax += inputList[idx]!!
+                calorieList[currGroup] = currMax
+            }
+        }
+
+        val topThree = calorieList.values.sortedDescending().take(3)
+        return topThree.sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    println(part1(input))
     println(part2(input))
 }
